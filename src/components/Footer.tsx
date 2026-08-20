a67dee697562e80a77fca43452803eb83ab27a01
haile <haileamlaksahle@gmail.com>
2026-08-20 21:57:34 +0300

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   const socialLinks = [
     { icon: Github, href: "https://github.com/0xh4il3x", label: "GitHub" },
     { icon: Linkedin, href: "https://linkedin.com/in/haileamlaksahle", label: "LinkedIn" },
-    { icon: Twitter, href: "https://x.com/Sahle_H", label: "Twitter" },
+    { icon: Twitter, href: "https://x.com/#", label: "Twitter" },
   ];
 
   return (
